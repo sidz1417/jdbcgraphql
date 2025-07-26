bed388ae493fd2e5f400cb71176e99e0bdf82970
Siddharth <sidz1408@gmail.com>
2025-07-26 16:02:48 +1000

    initial commit

---
+++ src/main/java/com/demo/jdbcgraphql/JdbcgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jdbcgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcgraphqlApplication.class, args);
+	}
+
+}
